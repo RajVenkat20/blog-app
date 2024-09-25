6d04e47db341ce0c67f85f176e4375272a6618ca
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2024-09-25 09:22:15 -0400

    Modified the Article.js file so that each article can be loaded

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,8 +1,20 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 // describing them as function components and not as class components as they are outdated practices
 const Article = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is the article page!</h1>
-    )
+      // Wrapping inside a React fragment so that we can return multiple elements
+      <>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph) => (
+          <p>{paragraph}</p>
+        ))}
+      </>
+    );
 }
 
 export default Article;
