ad1c61903877b55583d6ccf4162a205896c93ec1
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2024-10-06 16:05:49 -0400

    Added code for Education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,23 @@
-const Education = () => {
-
+const Education = ({ degree, university, yearRange, coursework }) => {
+    return (
+      <>
+        <div class="outermost-div">
+          <div class="second-div">
+            <div class="column left-column">
+              <p><b>{degree} <br/></b></p>
+              <span className="univName">{university}</span>
+            </div>
+            <div class="column right-column">
+              <p>{yearRange}</p>
+            </div>
+          </div>
+          <div class="coursework">
+            <p><b>Relevant Coursework</b>: <span style={{ fontStyle: 'italic' }}>{coursework}</span></p>
+          </div>
+        </div>
+        
+      </>
+    );
 }
 
 export default Education;
