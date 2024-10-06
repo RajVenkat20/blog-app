97b7e516643bff2fb5d33b34aa42099379611ef1
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2024-10-06 17:05:17 -0400

    Added image for 404: Not Found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,14 @@
+import errorImage from "../images/NotFound.png";
+
 // describing them as function components and not as class components as they are outdated practices
-const NotFound = () => (
-    <h1>404: Page Not Found</h1>
-    )
+const NotFound = () => {
+    return (
+        <>
+            <div className="not-found-container">
+                <img src={errorImage} alt="404 Error - Page Not Found" className="not-found-image" />
+            </div>
+        </>
+    );
+};
 
 export default NotFound;
